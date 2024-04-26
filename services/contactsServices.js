4a7ac9a44250cde7dfd23f4af88649d159f18310
Liubov2909 <ostapcuk2909@gmail.com>
2024-04-26 18:01:28 +0300

    dz2 node

---
+++ services/contactsServices.js
@@ -0,0 +1,46 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { nanoid } from "nanoid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "../db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone }
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
